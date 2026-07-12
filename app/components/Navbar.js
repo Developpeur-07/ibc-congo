a3386cb8474f4a173a223b616526c6313c2bcf87
Developpeur-07 <jesusnetng@gmail.com>
2026-07-12 23:42:04 +0100

    feat: logo

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -178,6 +178,7 @@ export default function Navbar() {
                 width: "auto",
                 height: "38px",
                 objectFit: "contain",
+                borderRadius: "6px",
               }}
             />
           </div>
